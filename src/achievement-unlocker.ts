d77ed6b06843756a78af2b24928d6aa97fdb4505
remvst <remvst@gmail.com>
2025-07-07 20:13:04 -0400

    Add achievement status API

--- src/achievement-unlocker.ts
+++ src/achievement-unlocker.ts
@@ -1,4 +1,11 @@
+export enum AchievementStatus {
+    IN_PROGRESS = "in_progress",
+    UNLOCKED = "unlocked",
+    FAILED = "failed",
+}
+
 export interface AchievementUnlocker {
     unlock(achievementId: string): void;
     fail(achievementId: string): void;
+    status(achievementId: string): AchievementStatus;
 }
